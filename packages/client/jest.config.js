dea4f909be5bca539cb5908415d22b1a4b3f7320
Alena I <elaineirr.910@gmail.com>
2023-02-10 21:12:45 +0400

    fix: fix jest errors

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -7,7 +7,7 @@ export default {
   testEnvironment: 'jsdom',
   testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
   moduleNameMapper: {
-    '\\.css$': '<rootDir>/jest/__mocks__/styleMock.js',
+    '\\.(css)$': '<rootDir>/jest/__mocks__/styleMock.ts',
   },
   globals: {
     __SERVER_PORT__: process.env.SERVER_PORT,
--- packages/client/src/components/MainLayout/index.tsx
+++ packages/client/src/components/MainLayout/index.tsx
@@ -1,11 +1,11 @@
-import React, { ReactNode } from 'react'
+import React, { Component, ReactNode } from 'react'
 import './index.css'
 
 type MainLayoutProps = {
   children: ReactNode
 }
 
-export class MainLayout extends React.Component<MainLayoutProps> {
+export class MainLayout extends Component<MainLayoutProps> {
   render() {
     return <div className="layout">{this.props.children}</div>
   }
