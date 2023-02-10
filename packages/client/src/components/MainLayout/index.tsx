21752175bb9f67c74f0b90a7875749eca9a8f90f
Alena I <elaineirr.910@gmail.com>
2023-02-10 20:48:32 +0400

    fix: fix Main Layout

--- packages/client/src/components/MainLayout/index.tsx
+++ packages/client/src/components/MainLayout/index.tsx
@@ -1,12 +1,10 @@
-import React, { ReactNode } from 'react'
+import { ReactNode } from 'react'
 import './index.css'
 
 type MainLayoutProps = {
   children: ReactNode
 }
 
-export class MainLayout extends React.Component<MainLayoutProps> {
-  render() {
-    return <div className="layout">{this.props.children}</div>
-  }
+export function MainLayout({ children }: MainLayoutProps) {
+  return <div className="layout">{children}</div>
 }
