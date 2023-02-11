8681622af77dd01077addc78ec83715e9da47e52
Alena I <elaineirr.910@gmail.com>
2023-02-11 16:57:26 +0400

    refactor: rename createButton test function

--- packages/client/src/components/Button/Button.test.tsx
+++ packages/client/src/components/Button/Button.test.tsx
@@ -5,30 +5,30 @@ import { Button } from './index'
 
 const BUTTON_TEXT = 'Button text'
 
-const createButton = (props?: Omit<ButtonProps, 'text'>) =>
+const renderButton = (props?: Omit<ButtonProps, 'text'>) =>
   render(<Button text={BUTTON_TEXT} {...props} />)
 
 describe('Button component', () => {
   it('Correctly renders in the DOM', () => {
-    createButton()
+    renderButton()
     expect(screen.getByRole('button')).toBeDefined()
     expect(screen.getByText(BUTTON_TEXT)).toBeDefined()
   })
 
   it('Correctly uses view prop', () => {
-    createButton({ view: 'secondary' })
+    renderButton({ view: 'secondary' })
     expect(screen.getByText(BUTTON_TEXT)).toHaveClass('button_view_secondary')
   })
 
   it('Correctly uses onClick prop', async () => {
     const handleClick = jest.fn()
-    createButton({ onClick: handleClick })
+    renderButton({ onClick: handleClick })
     await userEvent.click(screen.getByText(BUTTON_TEXT))
     expect(handleClick).toHaveBeenCalledTimes(1)
   })
 
   it('Could be disabled', () => {
-    createButton({ disabled: true })
+    renderButton({ disabled: true })
     expect(screen.getByText(BUTTON_TEXT)).toBeDisabled()
   })
 })
