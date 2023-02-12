029311646e968f5dc26b946763571ff5917cb715
Alena I <elaineirr.910@gmail.com>
2023-02-12 12:28:38 +0400

    test: add more tests

--- packages/client/src/components/Input/Input.test.tsx
+++ packages/client/src/components/Input/Input.test.tsx
@@ -1,10 +1,12 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import type { InputProps } from './index'
 import { Input } from './index'
 
 const INPUT_NAME = 'test-input'
 const INPUT_LABEL = 'test-label'
 const INPUT_ERROR = 'test-error'
+const TEST_VALUE = 'test-value'
 
 const renderInput = (props?: Omit<InputProps, 'name'>) =>
   render(<Input name={INPUT_NAME} {...props} />)
@@ -14,6 +16,15 @@ describe('Input component', () => {
     renderInput({ label: INPUT_LABEL })
     expect(screen.getByRole('textbox')).toBeDefined()
     expect(screen.getByText(INPUT_LABEL)).toBeDefined()
+  })
+
+  it('Correctly changes value with onChange prop', async () => {
+    const handleChange = jest.fn()
+    renderInput({ onChange: handleChange })
+    const inputElement = screen.getByRole('textbox')
+    await userEvent.type(inputElement, TEST_VALUE)
+    expect(handleChange).toHaveBeenCalledTimes(TEST_VALUE.length)
+    expect(inputElement).toHaveValue(TEST_VALUE)
   })
 
   it('Correctly shows error', () => {
