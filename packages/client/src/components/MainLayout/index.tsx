8ae95e475701fc6d8f4d28bc290d7baa9333d133
Юлия <63160106+uliasavi@users.noreply.github.com>
2023-02-27 12:46:19 +0300

    22 5/forum (#17)
    * 22-5 add forum & topic pages
    
    * 22-5 main-layout + forum after review
    
    * 22-5 pravki p->div
    
    ---------
    
    Co-authored-by: savitskaya <savitskaya@ap-team.ru>

--- packages/client/src/components/MainLayout/index.tsx
+++ packages/client/src/components/MainLayout/index.tsx
@@ -2,9 +2,13 @@ import { Component, ReactNode } from 'react'
 import HeaderComponent from '../Header'
 import { RoutesNameList } from '../../constant'
 import mainBack from '../../assets/images/background/Main.png'
+import mainBack2x from '../../assets/images/background/Main@2x.png'
 import profileBack from '../../assets/images/background/Profile.png'
+import profileBack2x from '../../assets/images/background/Profile@2x.png'
 import loginBack from '../../assets/images/background/Login.png'
+import loginBack2x from '../../assets/images/background/Login@2x.png'
 import leaderboardBack from '../../assets/images/background/Leaderboard.png'
+import leaderboardBack2x from '../../assets/images/background/Leaderboard@2x.png'
 import './index.css'
 
 type MainLayoutProps = {
@@ -12,25 +16,58 @@ type MainLayoutProps = {
   background: string
 }
 
-export class MainLayout extends Component<MainLayoutProps> {
+export class MainLayout extends Component<
+  MainLayoutProps,
+  { backgroundImage: string; backgroundImage2x: string; pageName: string }
+> {
   imageNames: { [key: string]: string } = {
     [RoutesNameList.Main]: mainBack,
-    [RoutesNameList.Forum]: mainBack,
     [RoutesNameList.Login]: loginBack,
     [RoutesNameList.Registration]: loginBack,
     [RoutesNameList.Leaderboard]: leaderboardBack,
     [RoutesNameList.Profile]: profileBack,
   }
-  getBackgroundImgPath(): string {
-    return this.imageNames[this.props.background]
+  imageNames2x: { [key: string]: string } = {
+    [RoutesNameList.Main]: mainBack2x,
+    [RoutesNameList.Login]: loginBack2x,
+    [RoutesNameList.Leaderboard]: leaderboardBack2x,
+    [RoutesNameList.Profile]: profileBack2x,
   }
+  constructor(props: MainLayoutProps) {
+    super(props)
+    this.state = {
+      backgroundImage: '',
+      backgroundImage2x: '',
+      pageName: this.props.background,
+    }
+  }
+
+  getBackgroundImgPath(image: string) {
+    this.setState({
+      backgroundImage: this.imageNames[image],
+      backgroundImage2x: this.imageNames2x[image],
+    })
+  }
+
+  componentDidMount() {
+    this.getBackgroundImgPath(this.props.background)
+  }
+
+  componentWillReceiveProps(nextProps: { background: string }) {
+    this.getBackgroundImgPath(nextProps.background)
+  }
+
   render() {
     return (
-      <div
-        className="layout"
-        style={{
-          backgroundImage: `url(${this.getBackgroundImgPath()})`,
-        }}>
+      <div className="layout">
+        {this.state.backgroundImage && (
+          <div className="layout__background-image">
+            <img
+              src={this.state.backgroundImage}
+              srcSet={`${this.state.backgroundImage2x} 2x`}
+            />
+          </div>
+        )}
         <div className="main-wrapper">
           <HeaderComponent />
           <div className="content-wrapper">{this.props.children}</div>
