93d1a88be3d3bb1d1d913e5b09c653e765ac9933
Alena I <elaineirr.910@gmail.com>
2023-03-20 21:30:24 +0400

    feat: update leaderboard components to display api data

--- packages/client/src/components/Leaderboard/index.tsx
+++ packages/client/src/components/Leaderboard/index.tsx
@@ -1,27 +1,50 @@
-import './index.css'
 import { Button } from '../Button'
 import { LeaderView } from './LeaderView'
 import { Title } from '../Title'
-import { leaderboardList } from './leaderboardList'
+import type { LeaderboardListRecord } from '../../api/leaderboard/types'
+import './index.css'
+
 const title = 'лидерборд'
-export function LeaderboardTable() {
+
+type LeaderboardTableProps = {
+  user?: LeaderboardListRecord | null
+  leaderboardList: LeaderboardListRecord[]
+}
+
+export function LeaderboardTable({
+  user,
+  leaderboardList,
+}: LeaderboardTableProps) {
   return (
     <div className="board">
-      <Title className={'board__title'} text={title} />
+      <Title className="board__title" text={title} />
+
       <div className="board__info">
         <div>№</div>
         <div>Игрок</div>
         <div>Результат</div>
       </div>
-      <div className="board__user">
-        <LeaderView order={8} avatar={''} username={'Вы'} score={278} />
-      </div>
+
+      {user && (
+        <div className="board__user">
+          <LeaderView
+            order={user.order}
+            avatar={user.avatar}
+            username="Вы"
+            score={user.score}
+          />
+        </div>
+      )}
+
       <div className="board__list">
-        {leaderboardList.map(player => (
-          <LeaderView key={player.order} {...player} />
-        ))}
+        {leaderboardList.map(player => {
+          if (player.id !== user?.id) {
+            return <LeaderView key={player.id} {...player} />
+          }
+        })}
       </div>
-      <Button className="board__button" text={'Начать игру'} />
+
+      <Button className="board__button" text="Начать игру" />
     </div>
   )
 }
