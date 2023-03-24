335fa94c5811d8ac8ab56559535a2ecbfed10fb6
savitskaya <savitskaya@ap-team.ru>
2023-03-24 13:53:09 +0300

    попытка 2

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
+import { fileURLToPath, URL } from 'url'
 dotenv.config()
 
 // https://vitejs.dev/config/
@@ -12,4 +13,16 @@ export default defineConfig({
     __SERVER_PORT__: process.env.SERVER_PORT,
   },
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url)),
+      },
+      {
+        find: '@assets',
+        replacement: fileURLToPath(new URL('./src/assets', import.meta.url)),
+      },
+    ],
+  },
 })
