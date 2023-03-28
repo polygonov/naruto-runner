abbccd87496a8510ba58fa3b8fad703eea64cc75
savitskaya <savitskaya@ap-team.ru>
2023-03-28 19:30:36 +0300

    еще правки к дев сборке ssr

--- packages/client/ssr.tsx
+++ packages/client/ssr.tsx
@@ -1,53 +1,17 @@
 import App from './src/App'
 import ReactDOMServer from 'react-dom/server'
+import { store } from './src/store'
+import { BrowserRouter } from 'react-router-dom'
 import React from 'react'
+import { Provider } from 'react-redux'
 
 export function render() {
-  return ReactDOMServer.renderToString(<App />)
-}
-
-/* import { renderToString } from 'react-dom/server'
-import type { Request, Response } from 'express'
-import { StaticRouter } from 'react-router-dom/server'
-import { Provider as ReduxProvider } from 'react-redux'
-import { store } from './src/store'
-import App from './src/App'
-import React from 'react'
-
-export default (req: Request, res: Response) => {
-  const location = req.url
-  const jsx = (
-    <ReduxProvider store={store}>
-      <StaticRouter location={location}>
+  const app = (
+    <Provider store={store}>
+      <BrowserRouter>
         <App />
-      </StaticRouter>
-    </ReduxProvider>
+      </BrowserRouter>
+    </Provider>
   )
-  const reactHtml = renderToString(jsx)
-  //const reduxState = store.getState()
-
-  if (location) {
-    res.redirect(location)
-    return
-  }
-
-  res.status(req.statusCode || 200).send(getHtml(reactHtml))
+  return ReactDOMServer.renderToString(app)
 }
-
-function getHtml(reactHtml: string) {
-  return `
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <title>NarutoRunner</title>
-        </head>
-        <body>
-            <div id="mount">${reactHtml}</div>
-        </body>
-        </html>
-    `
-}
- */
