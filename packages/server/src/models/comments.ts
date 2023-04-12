21a3b1ee9f6b9bd0d427a5226429122e8dd63e0f
Vladislav Mescheryakov <71271106+vmescher@users.noreply.github.com>
2023-04-12 22:36:13 +0500

    Feature/22 101 api forum (#32)
    * feat(server) api forum

---
+++ packages/server/src/models/comments.ts
@@ -0,0 +1,64 @@
+import {
+  DataType,
+  Column,
+  AutoIncrement,
+  Model,
+  PrimaryKey,
+  Table,
+  AllowNull,
+  ForeignKey,
+  Index,
+} from 'sequelize-typescript'
+import { Topic } from './topics'
+import type { Optional } from 'sequelize'
+
+type CommentAttributes = {
+  id: number
+  message: string
+  topic_id: number
+  authorId: number
+  status: boolean
+}
+
+type CommentCreationAttributes = Optional<CommentAttributes, 'id' | 'status'>
+
+@Table({
+  tableName: 'comments_table',
+  timestamps: true,
+  updatedAt: false,
+})
+export class Comment extends Model<
+  CommentAttributes,
+  CommentCreationAttributes
+> {
+  @AutoIncrement
+  @PrimaryKey
+  @Column(DataType.INTEGER)
+  override id: number
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  message: string
+
+  @Index
+  @ForeignKey(() => Topic)
+  @AllowNull(false)
+  @Column({
+    type: DataType.INTEGER,
+  })
+  topic_id: number
+
+  @AllowNull(false)
+  @Column({
+    type: DataType.INTEGER,
+    field: 'author_id',
+  })
+  authorId: number
+
+  @AllowNull(false)
+  @Column({
+    type: DataType.BOOLEAN,
+    defaultValue: true,
+  })
+  status: boolean
+}
