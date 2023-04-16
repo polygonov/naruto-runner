46a3b4a54d5986468bd848e3ebb4a17494ab69f0
savitskaya <savitskaya@ap-team.ru>
2023-04-16 19:35:13 +0300

    some changes

--- packages/client/ssr.tsx
+++ packages/client/ssr.tsx
@@ -1,17 +1,18 @@
 import App from './src/App'
 import { renderToString } from 'react-dom/server'
-import { store } from './src/store'
 import React from 'react'
 import { Provider } from 'react-redux'
 import { StaticRouter } from 'react-router-dom/server'
 import { configureStore } from '@reduxjs/toolkit'
+import { checkAuth } from './src/store/auth/thunk'
 
 export const render = async (url: string, storeData: any) => {
   const store = configureStore(storeData)
-
-  //await store.dispatch(getCurrentUser());
+  console.log(storeData)
+  await store.dispatch(checkAuth())
 
   const initialState = store.getState()
+
   const html = renderToString(
     <React.StrictMode>
       <Provider store={store}>
