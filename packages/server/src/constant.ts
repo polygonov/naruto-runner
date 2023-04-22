ba3fc7b199cc7a3dec6d1936751077eaec848ea0
vmescher <mescher174@gmail.com>
2023-04-22 21:38:08 +0500

    fix(server) Поправил cors и пути запросов

--- packages/server/src/constant.ts
+++ packages/server/src/constant.ts
@@ -1,1 +1,5 @@
+import * as process from 'process'
+
 export const PRACTICUM_ORIGIN = 'https://ya-praktikum.tech'
+
+export const WHITE_LIST = [`http://localhost:${process.env.CLIENT_PORT}`]
--- packages/server/src/routers/usersRouter.ts
+++ packages/server/src/routers/usersRouter.ts
@@ -4,7 +4,7 @@ import { UsersController } from '../controllers/usersController'
 export const usersRouter = (router: Router) => {
   const usersRouter = Router()
 
-  usersRouter.post('/update-theme/', UsersController.updateUserTheme)
+  usersRouter.post('/update-theme', UsersController.updateUserTheme)
 
   router.use('/user', usersRouter)
 }
